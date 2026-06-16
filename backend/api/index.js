a365583be9e17604c5172d10a4f2bfd60c76af2b
MrAnas07 <anusamir45@gamil.com>
2026-06-16 21:09:41 +0500

    Fix core error handling matrix and elevate CORS priority rank

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,9 +1,3 @@
 import app from "../src/app.js";
-import { connectDatabase } from "../src/config/database.js";
 
-const handler = async (req, res) => {
-  await connectDatabase();
-  return app(req, res);
-};
-
-export default handler;
+export default app;
